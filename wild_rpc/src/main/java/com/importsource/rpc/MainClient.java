eab67f9808ce300b3814679c367d6d585a20a481
Hezf <Hezf@Hezf-PC>
2016-03-28 09:07:09 +0800

    添加实现

--- wild_rpc/src/main/java/com/importsource/rpc/MainClient.java
+++ wild_rpc/src/main/java/com/importsource/rpc/MainClient.java
@@ -1,13 +1,15 @@
 package com.importsource.rpc;
 
-import com.importsource.rpc.op.Echo;
 import com.importsource.rpc.op.XService;
 
 public class MainClient {
 	public static void main(String[] args) {
-		Echo echo = RPC.getProxy(Echo.class, "127.0.0.1", 20382);
-		XService xService=RPC.getProxy(XService.class, "127.0.0.1", 20382);
-		System.out.println(echo.echo("hello,hello"));
-		System.out.println(xService.getCpu());
+		for(int i=0;i<100;i++){
+			//Echo echo = RPC.getProxy(Echo.class, "127.0.0.1", 20382);
+			XService xService=RPC.getProxy(XService.class, "127.0.0.1", 20382);
+			//System.out.println(echo.echo("hello,hello"));
+			System.out.println(xService.getCpu());
+		}
+		
 	}
 }
