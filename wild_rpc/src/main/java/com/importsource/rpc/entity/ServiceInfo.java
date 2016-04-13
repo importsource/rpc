a2fe9fbf4a11873635734d21ef0aef9bb9bd96e4
Hezf <Hezf@Hezf-PC>
2016-04-13 10:23:43 +0800

    加注释

--- wild_rpc/src/main/java/com/importsource/rpc/entity/ServiceInfo.java
+++ wild_rpc/src/main/java/com/importsource/rpc/entity/ServiceInfo.java
@@ -1,28 +1,44 @@
 package com.importsource.rpc.entity;
 /**
- * 注册时用到的服务类
+ * 注册时用到的服务bean
+ * 
  * @author Hezf
  *
- * @param <T>
  */
 @SuppressWarnings("rawtypes")
 public class ServiceInfo {
 	
 	private Class interfaceDefine;
 	private Class implDefine;
-
+    /**
+     * 得到服务接口定义类
+     * @return Class 服务接口定义类
+     */
 	public Class getInterfaceDefine() {
 		return interfaceDefine;
 	}
-
+    
+	/**
+	 * 设置服务接口定义类
+	 * @param interfaceDefine 服务接口定义类
+	 */
 	public void setInterfaceDefine(Class interfaceDefine) {
 		this.interfaceDefine = interfaceDefine;
 	}
 
+	
+	/**
+	 * 得到服务实现定义类
+	 * @return Class 服务实现定义类
+	 */
 	public Class getImplDefine() {
 		return implDefine;
 	}
 
+	/**
+	 * 设置服务实现定义类
+	 * @param implDefine 服务实现定义类
+	 */
 	public void setImplDefine(Class implDefine) {
 		this.implDefine = implDefine;
 	}
